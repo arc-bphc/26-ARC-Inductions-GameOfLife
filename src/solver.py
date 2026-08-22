633c9ca57bf2af36b565bcf773b51edfb286e4c3
Dark170707 <srinjoysaha2007@gmail.com>
2026-08-22 17:05:00 +0530

    SRINJOY SAHA [2026A3PS0371H]

--- src/solver.py
+++ src/solver.py
@@ -14,8 +14,16 @@ def count_neighbors(grid, row, col):
     """
     
     alive_count = 0
-    
-    # TODO: Implement your neighbor-counting logic here!
+    for x in [-1, 0, 1]:
+        for y in [-1, 0, 1]:
+            if x == 0 and y == 0:
+                continue
+            n_row = row + x
+            n_col = col + y
+
+            if 0 <= n_row < len(grid) and 0 <= n_col < len(grid[0]):
+                if grid[n_row][n_col] == 1:
+                    alive_count += 1
 
     return alive_count
 
@@ -42,8 +50,15 @@ def compute_next_generation(grid):
     # Create a new blank grid of the same size, filled with 0s (dead cells)
     next_grid = [[0 for _ in range(cols)] for _ in range(rows)]
     
-    # TODO: Iterate through every cell in the `grid`.
-    # TODO: Use your `count_neighbors` function to find out how many neighbors it has.
-    # TODO: Apply the 4 Rules of Life to determine if it should be 1 (alive) or 0 (dead) in `next_grid`.
+    for row in range(rows):
+        for col in range(cols):
+
+            alive_neighbors = count_neighbors(grid, row, col)
+            if grid[row][col] == 1:
+                if alive_neighbors == 2 or alive_neighbors == 3:
+                    next_grid[row][col] = 1
+            else:
+                if alive_neighbors == 3:
+                    next_grid[row][col] = 1
 
     return next_grid
