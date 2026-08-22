60b64b48fb41a5c63834f9bf445d33f8a5e472d9
Rushvith <rushvithanthagiri@gmail.com>
2026-08-22 15:57:26 +0530

    Finish Task 2: completed compute_next_generation in solver.py

--- src/solver.py
+++ src/solver.py
@@ -71,4 +71,16 @@ def compute_next_generation(grid):
     # TODO: Use your `count_neighbors` function to find out how many neighbors it has.
     # TODO: Apply the 4 Rules of Life to determine if it should be 1 (alive) or 0 (dead) in `next_grid`.
 
+    for r in range(rows):
+        for c in range(cols):
+            alive_count = count_neighbors(grid, r, c)
+            if alive_count < 2: # rule 1: underpopulation - live cell dies, dead cell stays dead
+                next_grid[r][c] = 0
+            elif alive_count == 3: # rule 4: reproduction - live cell survives, dead cell becomes live
+                next_grid[r][c] = 1
+            elif alive_count > 3: # rule 3: overpopulation - live cell dies, dead cell stays dead
+                next_grid[r][c] = 0
+            elif alive_count == 2 and grid[r][c] == 1: # rule 2: survival - live cell survives, dead cell stays dead
+                next_grid[r][c] = 1
+
     return next_grid
